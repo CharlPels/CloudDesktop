b92a80bd4a24c2a7115a671e51b85eba12703d69
CharlPels <charl@cpels.nl>
2017-03-19 15:22:44 +0100

    Switch to use azure functions

--- standaloneRDPGatewayConfig.ps1
+++ standaloneRDPGatewayConfig.ps1
@@ -1,23 +1,44 @@
 [string]$cpelsEmail = $args[0]
 
+function check-servercertificate($ServerToCheck)
+{
+    $timeoutMilliseconds = 9000
+    $url = "https://$ServerToCheck"
+    #disabling the cert validation check.
+    [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+    #Create request
+    $req = [Net.HttpWebRequest]::Create($url)
+    $req.Timeout = $timeoutMilliseconds
+    try {$req.GetResponse() |Out-Null} catch 
+    {
+        [System.Net.ServicePointManager]::ServerCertificateValidationCallback = $null
+        return "error"
+    }
+    $certThumbprint = $req.ServicePoint.Certificate.GetCertHashString()
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = $null
+    return $certThumbprint 
+}
 
 $RegistryLocation = "HKLM:\Software\CloudDesktop"
 
 #Get Server public IP
 #$getIP = (Invoke-RestMethod -Method "get" -Uri "localhost/backend/api/v1.0/get-pubip" -ContentType application/json).ip
+
+#We will first check if this is the first time the script runs
 if ((Get-ItemProperty -Path $RegistryLocation -Name FirstRun -ErrorAction SilentlyContinue).firstrun)
 {$firstrun=$false}
 else
 {
 	mkdir $RegistryLocation
+    #if script runs manualy we ask for the email address we need to send the connaection information to
 	if ($cpelsEmail.Length -eq 0) {$cpelsEmail = Read-Host 'What is your email address?'} else {sleep 60}
     Set-ItemProperty -Path $RegistryLocation -Name FirstRun -Value "done"
     $firstrun=$true
 }
 
 if ($cpelsEmail.Length -gt 0 -and $cpelsEmail -ne "-Confirm:") {Set-ItemProperty -Path $RegistryLocation -Name cpelsEmail -Value $cpelsEmail} else {$cpelsEmail = (Get-ItemProperty -Path $RegistryLocation -Name cpelsEmail -ErrorAction SilentlyContinue).cpelsEmail}
-$cpelsSerial = (Get-ItemProperty -Path $RegistryLocation -Name cpelsSerial -ErrorAction SilentlyContinue).cpelsSerial
-if($cpelsSerial) {write-host "serial found"} else {$cpelsSerial =  ""}
+#$cpelsSerial = (Get-ItemProperty -Path $RegistryLocation -Name cpelsSerial -ErrorAction SilentlyContinue).cpelsSerial
+#if($cpelsSerial) {write-host "serial found"} else {$cpelsSerial =  ""}
 
 #Download this script for running everytime on system startup
 if ((Test-Path "c:\support") -eq $false){mkdir "c:\support"}
@@ -38,62 +59,38 @@ if ((Test-Path $destination) -eq $false)
 }
 
 
+#The request we send to the API for certificate and DNS updates
 $body = @{
-    'email'= $cpelsEmail
-    'serial'= $cpelsSerial
+    'Email'= $cpelsEmail
 }
 
 ConvertTo-Json $body
-$response = Invoke-RestMethod -Method "post" -Uri "https://mydesktop-api.cpels.com/mydesktop/api/v1.0/createupdatedns" -Body (ConvertTo-Json $body) -ContentType application/json
+try{
+$response = Invoke-RestMethod -Method "post" -Uri "https://mydesktopfunction.azurewebsites.net/mydesktop/api/v1.0/createupdatedns" -Body (ConvertTo-Json $body) -ContentType application/json
 
-$source = $response.info.keysource
+$response = ConvertFrom-Json $response
+}
+catch
+{write-host "error in communication"
+break}
+
+$source = $response.keysource
 $certdestination = "c:\support\key.p12"
 Invoke-WebRequest $source -OutFile $certdestination
-$mypwd = ConvertTo-SecureString -String $response.info.keypass -Force -AsPlainText 
+$mypwd = ConvertTo-SecureString -String $response.keypass -Force –AsPlainText
+try{
 $Thumbprint = Import-PfxCertificate –FilePath $certdestination cert:\localMachine\my -Password $mypwd -Confirm:$false
+}
+catch {
+#We need to break if certifacte instalation fails
+write-host "certificate import error"
+break}
 
-$HostName = $response.info.dnsname
+
+$HostName = $response.DNSName
 if ((Test-Path $certdestination) -eq $TRUE) {rm $certdestination -Force}
-Set-ItemProperty -Path $RegistryLocation -Name cpelsSerial -Value $response.info.serial
+#Set-ItemProperty -Path $RegistryLocation -Name cpelsSerial -Value $response.info.serial
     
-
-###############################################
-#Check to see script has administrator access
-###############################################
-#http://blogs.msdn.com/b/virtual_pc_guy/archive/2010/09/23/a-self-elevating-powershell-script.aspx
-# Get the ID and security principal of the current user account
-$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
- 
-# Get the security principal for the Administrator role
-$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
- 
-# Check to see if we are currently running "as Administrator"
-if ($myWindowsPrincipal.IsInRole($adminRole))
-   {
-   # We are running "as Administrator" - so change the title and background color to indicate this
-   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
-   clear-host
-   }
-else
-   {
-   # We are not running "as Administrator" - so relaunch as administrator
-   # Create a new process object that starts PowerShell
-   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-   # Specify the current script path and name as a parameter
-   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
-   # Indicate that the process should be elevated
-   $newProcess.Verb = "runas";
-   # Start the new process
-   [System.Diagnostics.Process]::Start($newProcess);
-   # Exit from the current, unelevated, process
-   exit
-   }
- 
-# Run your code that needs to be elevated here
-
-
 
 
 if ((Get-WindowsFeature -Name RDS-Gateway).installed -eq $false) 
@@ -116,8 +113,8 @@ if (((Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion
     Set-WebConfigurationProperty -pspath "iis:\Sites\Default Web Site\RDWeb\" -filter "/appSettings/add[@key='WorkspaceName']" -name value -Value "My workplace in the cloud"
 
     #Cleanup existing RAP and CAP entries
-    get-item -Path "RDS:\GatewayServer\RAP\*" | remove-item -Recurse -Force -Confirm:$false
-    get-item -Path "RDS:\GatewayServer\cap\*" | remove-item -Recurse -Force -Confirm:$false
+    #get-item -Path "RDS:\GatewayServer\RAP\*" | remove-item -Recurse -Force -Confirm:$false
+    #get-item -Path "RDS:\GatewayServer\cap\*" | remove-item -Recurse -Force -Confirm:$false
 
     #Configure RDP Gateway RAP and CAP entries
     New-Item -Path "RDS:\GatewayServer\cap" -Name FullAccess -UserGroups "administrators@BUILTIN" -AuthMethod 1
@@ -131,30 +128,37 @@ if (((Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion
 	
 	Set-WebConfigurationProperty -pspath "iis:\Sites\Default Web Site\RDWeb\Pages" -filter "/appSettings/add[@key='DefaultTSGateway']" -name value -Value "$HostName"
 
-	
-	#Disable-IEESC
-	#sleep 50
-	
+    Restart-Computer -Force
+	break
 }
 
+#Configure RDP website
+Set-WebConfigurationProperty -pspath "iis:\Sites\Default Web Site\RDWeb\Pages" -filter "/appSettings/add[@key='DefaultTSGateway']" -name value -Value "$HostName"
 
+Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $Thumbprint.Thumbprint
+sleep 5
+#Restart Services
+#we restart the webserver every day, this is not the main goal but solve the installation issue
+#where the gateway is not picking up the new settings
+write-host "Restart Services"
+Restart-Service "W3SVC" -Force
+Restart-Service "remote desktop gateway" -Force
 
-
-if ($Thumbprint.Thumbprint -notlike (get-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint).CurrentValue)
+#Life is not perfect and certificate installation will fail sometimes that is what we check the actual server certificate to make shure all is oke
+if ((check-servercertificate -ServerToCheck localhost) -ne $Thumbprint.Thumbprint)
 {
-    Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $Thumbprint.Thumbprint
-	#Configure RDP website
-	Set-WebConfigurationProperty -pspath "iis:\Sites\Default Web Site\RDWeb\Pages" -filter "/appSettings/add[@key='DefaultTSGateway']" -name value -Value "$HostName"
+write-host "Stopping the gateway and webserver"
+Stop-Service "W3SVC" -Force
+Stop-Service "remote desktop gateway" -Force
 
-	#Restart Services
-    if ($firstrun -eq $false)
-    {
-	    Restart-Service "W3SVC" -Force
-	    Restart-Service "remote desktop gateway"
-    }
-    else
-    {
-        Restart-Computer -Force
-    }
-}
+Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $Thumbprint.Thumbprint
 
+#Restart Services
+#write-host "Restart Services"
+#Restart-Service "W3SVC" -Force
+#Restart-Service "remote desktop gateway" -Force
+
+Restart-Computer -Force
+
+
+} else {write-host "All oke now"}
